a1b03eb9ecccc37eee98b5e9157b29eda5e01657
fafa <ansmos@mail.ru>
2024-12-22 23:16:49 +0300

    completed

--- app/src/main/java/ru/ansmos/filmoteka/rw/FilmViewHolder.kt
+++ app/src/main/java/ru/ansmos/filmoteka/rw/FilmViewHolder.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import ru.ansmos.filmoteka.R
 import ru.ansmos.filmoteka.db.Film
+import ru.ansmos.filmoteka.decor.RatingDonutView
 
 //В конструктор класс передается layout, который мы создали(film_item.xml)
 class FilmViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -14,6 +15,8 @@ class FilmViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemV
     private val title = itemView.findViewById<TextView>(R.id.title)
     private val poster = itemView.findViewById<ImageView>(R.id.poster)
     private val desc = itemView.findViewById<TextView>(R.id.description)
+    //Вот здесь мы находим в верстке наш прогресс бар для рейтинга
+    private val ratingDonut = itemView.findViewById<RatingDonutView>(R.id.rating_donut)
 
     //В этом методе кладем данные из Film в наши View
     fun bund(film: Film){
@@ -27,5 +30,12 @@ class FilmViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemV
             //Указываем ImageView, куда будем загружать изображение
             .into(poster)
         desc.text = film.description
+        //Устанавливаем рэйтинг
+        ratingDonut.setProgress((film.rating * 10).toInt())
+        if (film.rating != 0f) {
+            ratingDonut.visibility = View.VISIBLE
+        } else {
+            ratingDonut.visibility = View.INVISIBLE
+        }
     }
 }
